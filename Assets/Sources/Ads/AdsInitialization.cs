59c49bbf59258b4c8f29b858cd425f8cbfe3dd7d
IvanGriboedov <educateGriboedov@gmail.com>
2023-05-12 21:06:50 +0300

    Created banner

---
+++ Assets/Sources/Ads/AdsInitialization.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+namespace Sources.Ads
+{
+	public class AdsInitialization : MonoBehaviour
+	{
+		[SerializeField] private AdSettings _settings;
+
+		[SerializeField] private AdUnitId _adUnitId;
+		
+		
+		public void Initialize()
+		{
+			Advertisement.Initialize(_settings.GameId, _settings.TestMode);
+			
+			Advertisement.Banner.SetPosition(BannerPosition.BOTTOM_CENTER);
+			Advertisement.Banner.Show(_adUnitId.Banner);
+		}
+	}
+}
--- Assets/Sources/Infrastructure/Bootstrap.cs
+++ Assets/Sources/Infrastructure/Bootstrap.cs
@@ -1,5 +1,6 @@
 ﻿using GameStates.Base;
 using GameStates.States;
+using Sources.Ads;
 using StaticContext;
 using UnityEngine;
 
@@ -8,9 +9,11 @@ namespace Infrastructure
 	public class Bootstrap : MonoBehaviour
 	{
 		[SerializeField] private GameStateMachineFactory _stateMachineFactory;
-
+		[SerializeField] private AdsInitialization _adsInitialization;
+		
 		private void OnEnable()
 		{
+			_adsInitialization.Initialize();
 			IGameStateMachine stateMachine = _stateMachineFactory.Initialize();
 
 			Instance<IGameStateMachine>.Value = stateMachine;
